d67f1a224fb00057c8ec723ab3b8244350c799c0
SomeoneIsHere-Code <74882050+SomeoneIsHere-Code@users.noreply.github.com>
2021-05-04 10:55:08 -0500

    Add files via upload

---
+++ Creative-Caterpillars-Code/main.cpp
@@ -0,0 +1,10 @@
+/* 
+Since I can't actually code in our arduino and don't know 
+what the inputs and outputs are I'm going to create as much of the
+logic today as I can while making it easy on myself to add in the pins
+and everything once we have that set in stone
+
+In my usual style I'm going to probably over comment and use way too 
+may variables and functions but it's how I code
+
+*/

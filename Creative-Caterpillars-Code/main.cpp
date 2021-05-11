a061fe5ed8ddd1cbe2a6a981556684cc6380730e
SomeoneIsHere-Code <74882050+SomeoneIsHere-Code@users.noreply.github.com>
2021-05-11 13:28:19 -0500

    Update main.cpp

--- Creative-Caterpillars-Code/main.cpp
+++ Creative-Caterpillars-Code/main.cpp
@@ -1,12 +1,23 @@
+void setup() {
+  //setting up all of the (placeholder) pins
+  pinMode(2,OUTPUT);
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
 /* 
 Since I can't actually code in our arduino and don't know 
 what the inputs and outputs are I'm going to create as much of the
 logic today as I can while making it easy on myself to add in the pins
 and everything once we have that set in stone
-
 In my usual style I'm going to probably over comment and use way too 
 many variables and functions but it's how I code
-
 I'm going to use alt to refer to altitude and to use the altimiter
 whereas I will use dist to refer to distance using the ultrasonic
 sensor
@@ -42,6 +53,9 @@ bool descending = false;
 //see if the button has been pressed
 int button = 0;
 
+//the delay between pictures being taken
+int picDelay = 0;
+
 //checks to see if the button has been pressed at all
 if (button == 1){
 
@@ -54,18 +68,18 @@ if (button == 1){
 
   //if the drone has reached the first picture height it takes a picture. It also has a small buffer area
   if(altTemp == firstPicAlt){
-    //turn servo
-    //wait some time
-    //stop turning servo
+    digitalWrite(2,HIGH);
+    delay(picDelay);
+    digitalWrite(2,LOW);
     numPics++;
   }
 
   //This one is a doozy. Essentially if the remainder from the distance after the firstPicAlt 
   //divided by the picDist is zero then it takes a picture
     if((altTemp - firstPicALt)%picdist == 0){
-    //turn servo
-    //wait some time
-    //stop turning servo 
+    digitalWrite(2,HIGH);
+    delay(picDelay);
+    digitalWrite(2,LOW); 
     numPics++;
   }
   
